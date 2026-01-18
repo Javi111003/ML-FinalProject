a98393a68b59cd7766f9004f742f98916e1b1366
Moist-Cat <lamatcardenas@gmail.com>
2026-01-18 09:41:50 -0500

    generalize and delete answer
            Added validation as well.

--- automl/components/comparison.py
+++ automl/components/comparison.py
@@ -124,7 +124,11 @@ class ModelComparison:
                 fig, ax = plt.subplots(figsize=(10, 4))
 
                 # Last 100 historical points
-                historical = series.iloc[-100 + forecast_steps:] if len(series) > 100 else series
+                historical = (
+                    series[series.columns[0]].iloc[-100 + forecast_steps :]
+                    if len(series) > 100
+                    else series
+                )
                 ax.plot(
                     historical.index,
                     historical.values,
@@ -136,8 +140,8 @@ class ModelComparison:
 
                 # Forecast
                 ax.plot(
-                        forecast.index[-100:],
-                        forecast.values[-100:],
+                    forecast.index[-100:],
+                    forecast.values[-100:],
                     label="Forecast",
                     color="red" if i == 0 else "blue",
                     linestyle="--",
@@ -173,8 +177,8 @@ class ModelComparison:
         fig, ax = plt.subplots(figsize=(12, 6))
 
         # Historical data
-        #historical = series.iloc[-100:] if len(series) > 100 else series
-        historical = series
+        # historical = series.iloc[-100:] if len(series) > 100 else series
+        historical = series[series.columns[0]]
         ax.plot(
             historical.index,
             historical.values,
